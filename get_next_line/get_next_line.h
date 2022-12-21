22102d8bd5996ddb9d16ae38776a6915e8c329e4
DongwookHong <hduk4902@naver.com>
2022-12-21 11:59:23 +0900

    fix

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: donghong <donghong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/18 21:17:46 by donghong          #+#    #+#             */
-/*   Updated: 2022/12/15 10:27:33 by donghong         ###   ########.fr       */
+/*   Created: 2022/12/21 11:58:51 by donghong          #+#    #+#             */
+/*   Updated: 2022/12/21 11:59:09 by donghong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-// void f(unsigned int a, char* abc)
-// {
-// 	(void)a;
-// 	*abc += 2;	
-// }
-
-void	ft_striteri(char *s, void (*f)(unsigned int, char *))
-{
-	size_t	i;
-
-	i = 0;
-	if (!s)
-		return ;
-	while (s[i])
-	{
-		(*f)(i, &s[i]);
-		i++;
-	}
-}

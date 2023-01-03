070bc7f530322316bd90e210ed045a767afe241e
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2023-01-03 16:36:13 +0900

    Update get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -10,3 +10,112 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*abc;
+	size_t	i;
+	size_t	length;
+
+	i = 0;
+	length = ft_strlen(s1);
+	abc = (char *)malloc(sizeof(char) * length + 1);
+	if (!abc)
+		return (0);
+	while (i < length)
+	{
+		abc[i] = s1[i];
+		i++;
+	}
+	abc[i] = '\0';
+	return (abc);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	size;
+	size_t	i;
+	char	*dest;
+
+	if (start > ft_strlen(s))
+		size = 0;
+	else if (len > ft_strlen(s + start))
+		size = ft_strlen(s + start);
+	else
+		size = len;
+	dest = (char *)malloc(sizeof(char) * (size + 1));
+	if (!dest)
+		return (0);
+	i = 0;
+	if (size)
+		while (i < len && s[start])
+			dest[i++] = s[start++];
+	dest[i] = '\0';
+	return (dest);
+}
+
+int ft_strchr(const char *s, int c)
+{
+	char	find;
+    int     i;
+
+    i = 0;
+	find = (char)c;
+	while (s[i] && s[i] != find)
+		i++;
+	if (s[i] == find)
+		return (i);
+	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*dest;
+
+	i = 0;
+	j = 0;
+	if (!(s1) || !(s2))
+		return (0);
+	dest = (char *)malloc(sizeof(char) *(ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dest)
+		return (0);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i] = s2[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*next1;
+
+	next1 = (t_list *)malloc(sizeof(t_list));
+	if (!next1)
+		return (0);
+	next1 -> content = content;
+	next1 -> next = NULL;
+	return (next1);
+}

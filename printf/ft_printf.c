79aa149733b4edd076f650a09e4162cd7bd31b82
DongwookHong <hduk4902@naver.com>
2023-03-15 20:32:25 +0900

    rew

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,11 +6,37 @@
 /*   By: donghong < donghong@student.42seoul.kr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 18:56:17 by donghong          #+#    #+#             */
-/*   Updated: 2023/03/15 20:14:57 by donghong         ###   ########.fr       */
+/*   Updated: 2023/03/15 20:30:11 by donghong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_check(char ch, va_list ap)
+{
+	int	cnt;
+
+	cnt = 0;
+	if (ch == 'c')
+		cnt += (ft_putchar(va_arg(ap, int)));
+	else if (ch == 's')
+		cnt += (ft_putstr(va_arg(ap, char *)));
+	else if (ch == 'd' || ch == 'i')
+		cnt += (ft_putdeci(va_arg(ap, int)));
+	else if (ch == 'u')
+		cnt += (ft_putunsigned(va_arg(ap, unsigned int)));
+	else if (ch == 'x')
+		cnt += (ft_hexlow(va_arg(ap, unsigned int)));
+	else if (ch == 'X')
+		cnt += (ft_hexupper(va_arg(ap, unsigned int)));
+	else if (ch == 'p')
+		cnt += (ft_pointer(va_arg(ap, size_t)));
+	else if (ch == '%')
+		cnt += (ft_putchar('%'));
+	else
+		return (-1);
+	return (cnt);
+}
 
 int	ft_printf(const char *format, ...)
 {

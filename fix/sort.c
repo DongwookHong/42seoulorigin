a8d378433a226e1d6aecb6526647e8e5bd35316b
DongwookHong <hduk4902@naver.com>
2023-04-19 16:02:23 +0900

    rew

--- fix/sort.c
+++ fix/sort.c
@@ -6,6 +6,45 @@ void change_b(t_list **b, int line)
         rb(b);
 }
 
+void three_min_sort(t_list **a)
+{ 
+    t_list *temp;
+
+    temp = (*a)->next;
+    if((*a)->order == 1&&  temp ->order ==3)
+    {
+        sa(a);
+        ra(a);
+    }
+    else if( (*a)->order == 2 &&  temp ->order ==1)
+        sa(a);
+    else if((*a)->order == 2 &&  temp ->order ==3)
+        rra(a);
+    else if((*a)->order == 3 &&  temp ->order ==1)
+        ra(a);
+    else if((*a)->order == 3 &&  temp ->order ==2)    
+    {
+        sa(a);
+        rra(a);
+    }
+}
+
+// int find_lenorder(t_list **b,int size, int len)
+// {
+//     t_list *top = *b;
+   
+//     int topnum=1;
+//     while(1)
+//     {
+//         if(top -> order == size)
+//             break;
+//         topnum++;
+//         top=top ->next;
+//     }
+//     if(topnum <= len)
+//         return 1;
+//     return 2;
+// }
 int find_lenorder(t_list **b,int size, int len)
 {
     t_list *top = *b;
@@ -13,7 +52,7 @@ int find_lenorder(t_list **b,int size, int len)
     int topnum=1;
     while(1)
     {
-        if(top -> order == size)
+        if(top -> order > size-1)
             break;
         topnum++;
         top=top ->next;
@@ -22,36 +61,45 @@ int find_lenorder(t_list **b,int size, int len)
         return 1;
     return 2;
 }
-// int find_lenorder2(t_list **b,int size, int len)
-// {
-//     t_list *top = *b;
-   
-//     int topnext=1;
-//     while(1)
-//     {
-//         if(top -> order-1 == size-1)
-//             break;
-//         topnext++;
-//         top=top ->next;
-//     }
-//     if(topnext <= len)
-//         return 1;
-//     return 2;
-// }
 
+// void sort_b(t_list **b,int size)
+// {
+//     int len = size/2;
+//     if((*b) ->order == size)
+//         return;
+//     int k = find_lenorder(b,size,len);
+//     // int y = find_lenorder2(b,size,len);
+//     if(k ==1)
+//     {
+//         while(1)
+//         {
+//             rb(b);
+//             if((*b) ->order == size)
+//                 return;
+//         }
+//     }
+//     else if(k ==2)
+//     {
+//         while(1)
+//         {
+//             rrb(b);
+//             if((*b) ->order == size)
+//                 return;
+//         }
+//     }
+// }
 void sort_b(t_list **b,int size)
 {
     int len = size/2;
     if((*b) ->order == size)
         return;
     int k = find_lenorder(b,size,len);
-    // int y = find_lenorder2(b,size,len);
     if(k ==1)
     {
         while(1)
         {
             rb(b);
-            if((*b) ->order == size)
+            if((*b) ->order > size-1)
                 return;
         }
     }
@@ -60,7 +108,7 @@ void sort_b(t_list **b,int size)
         while(1)
         {
             rrb(b);
-            if((*b) ->order == size)
+            if((*b) ->order > size-1)
                 return;
         }
     }
@@ -93,11 +141,14 @@ void sortatob(t_list **a,t_list **b,t_info info)
 void sortbtoa(t_list **a , t_list **b,t_info info)
 {
     int find_order = info.size;
-   
+    int cnt = 0;
     while(find_order != 0)
     { 
+        cnt = ft_lstsize(*a);
         sort_b(b ,find_order);
         pa(a,b);
+        if(cnt ==0)
+           three_min_sort(a);
         find_order--;
     }
 }

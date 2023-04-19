d3637224e1bdcc2fb657b7b97dc2820a93332f19
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2023-04-19 17:16:04 +0900

    Add files via upload

---
+++ pushswap/origin/command/reverse.c
@@ -0,0 +1,47 @@
+#include "../push_swap.h"
+
+void rra(t_list **a)
+{
+    t_list *tmp;
+    t_list *head;
+
+    head = (*a);
+    if (!head || !head->next)
+		return ;
+    while(head->next)
+    {
+        tmp= head;
+        head = head ->next;
+    }
+    tmp -> next = NULL;
+    head ->next= *a;
+    *a = head;
+    write(1,"rra\n",4);
+}
+
+
+void rrb(t_list **b)
+{
+  t_list *tmp;
+  t_list *head;
+
+  head = (*b);
+  if(!head || !head->next)
+    return ;
+  while(head ->next)
+  {
+    tmp = head;
+    head = head ->next;
+  }
+  tmp ->next =NULL;
+  head->next =*b;
+  *b = head;
+  write(1,"rrb\n",4);
+}
+
+void rrr(t_list **a,t_list **b)
+{
+  rra(a);
+  rrb(b);
+  write(1,"rrr\n",4);
+}
---
+++ pushswap/origin/sort.c
@@ -0,0 +1,108 @@
+#include "push_swap.h"
+
+void change_b(t_list **b, int line)
+{
+    if(line >=(*b)->num)
+        rb(b);
+}
+
+int find_lenorder(t_list **b,int size, int len)
+{
+    t_list *top = *b;
+   
+    int topnum=1;
+    while(1)
+    {
+        if(top -> order == size)
+            break;
+        topnum++;
+        top=top ->next;
+    }
+    if(topnum <= len)
+        return 1;
+    return 2;
+}
+
+
+void sort_b(t_list **b,int size)
+{
+    int len = size/2;
+    if((*b) ->order == size)
+        return;
+    int k = find_lenorder(b,size,len);
+    if(k ==1)
+    {
+        while(1)
+        {
+            rb(b);
+            if((*b) ->order == size)
+                return;
+        }
+    }
+    else if(k ==2)
+    {
+        while(1)
+        {
+            rrb(b);
+            if((*b) ->order == size)
+                return;
+        }
+    }
+}
+
+
+
+// void sortatob(t_list **a,t_list **b,t_info info)
+// {
+//     int i =0;
+//     int num= info.chunk;
+//     int size = info.size;
+//     // printf("chunk의 값은 %d, size의값은 ?%d\n",num,size);
+    
+//     while(i<size)
+//     {   
+//         if((*a)->order <= num+i)
+//         {
+//             pb(a,b);
+//             change_b(b,i);
+//             i++;
+//         }
+//         if(*a == NULL)
+//             break;
+//         else
+//             ra(a);
+//     }
+// }
+void sortatob(t_list **a,t_list **b,t_info info)
+{
+    int i =0;
+    int num= info.chunk;
+    int size = info.size;
+    
+    while(i<size)
+    {   
+        if((*a)->order <= num+i)
+        {
+            pb(a,b);
+            change_b(b,i);
+            i++;
+        }
+        if(*a == NULL)
+            break;
+        else
+            ra(a);
+    }
+}
+
+
+void sortbtoa(t_list **a , t_list **b,t_info info)
+{
+    int find_order = info.size;
+   
+    while(find_order != 0)
+    { 
+        sort_b(b ,find_order);
+        pa(a,b);
+        find_order--;
+    }
+}

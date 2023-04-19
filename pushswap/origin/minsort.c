8b8299ae6d8bbe5fd5e82cadc7fafe19f044ed94
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2023-04-19 22:07:03 +0900

    Add files via upload

---
+++ pushswap/origin/minsort.c
@@ -0,0 +1,125 @@
+// #include "push_swap.h"
+
+
+// void three_sort(t_list **a)
+// { 
+//     t_list *temp;
+//     t_list *last;
+//     temp = (*a)->next;
+//     last =ft_lstlast(a);
+
+//     if((*a)->order < last->order &&  last->order < temp -> order )
+//     {
+//         sa(a);
+//         ra(a);
+//     }
+//     else if( temp-> order < (*a) -> order && (*a) -> order < last->order)
+//         sa(a);
+//     else if((*a)->order < temp-> order && (*a) -> order > last->order)
+//         rra(a);
+//     else if((*a)->order > last->order && temp-> order < last->order)
+//         ra(a);
+//      else if((*a)->order > temp->order && temp-> order > last->order)    
+//     {
+//         sa(a);
+//         rra(a);
+//     }
+// }
+
+
+// void five_sort(t_list **a,t_list **b)
+// {
+//     t_list *temp;
+//     temp = (*a)->next;
+//     pb(a,b);
+//     three_sort(a);
+//     pb(a,b);
+//     three_sort(a);
+//     while(1)
+//     {
+//         if(b != NULL)
+//             pa(a,b);   
+//         three_sort(a);
+//         if(ordercheck(a) ==1)
+//             break;
+//     }
+    
+// }
+
+
+
+// void minsort(t_list **a,t_list **b,t_info info)
+// {     
+//     if(info.size ==2)
+//         sa(a);
+//     else if(info.size ==3)
+//         three_sort (a)                                                                 ;
+//     else
+//         five_sort(a,b);
+//     return ;
+// }
+
+#include "push_swap.h"
+
+
+void three_sort(t_list **a)
+{ 
+    t_list *temp;
+    t_list *last;
+    temp = (*a)->next;
+    last =ft_lstlast(*a);
+
+    if((*a)->order < last->order &&  last->order < temp -> order )
+    {
+        sa(a);
+        ra(a);
+    }
+    else if( temp-> order < (*a) -> order && (*a) -> order < last->order)
+        sa(a);
+    else if((*a)->order < temp-> order && (*a) -> order > last->order)
+        rra(a);
+    else if((*a)->order > last->order && temp-> order < last->order)
+        ra(a);
+     else if((*a)->order > temp->order && temp-> order > last->order)    
+    {
+        sa(a);
+        rra(a);
+    }
+}
+
+void mintwo(t_list **a)
+{
+    sa(a);
+}
+
+
+
+void five_sort(t_list **a,t_list **b,t_info info)
+{
+    int cnt = info.size;
+    
+    three_sort(a);
+    pb(a,b);
+    three_sort(a);
+    pb(a,b);
+    three_sort(a);
+    if((*b) ->order < (*b) ->next ->order)
+        sb(b);
+    while(cnt-- >0)
+    {
+        pa(a,b);   
+        three_sort(a);
+    }
+}
+
+
+void minsort(t_list **a,t_list **b,t_info info)
+{     
+    if(info.size ==2)
+        mintwo(a);
+    else if(info.size ==3)
+        three_sort (a)                                                                 ;
+    else
+        five_sort(a,b,info);
+    return ;
+}

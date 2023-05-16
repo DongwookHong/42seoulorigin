6558c077229adcf6109d94a98ace622917ecfc1f
DongwookHong <hduk4902@naver.com>
2023-05-16 12:03:16 +0900

    rew

--- so_long/img.c
+++ so_long/img.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   img.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donghong < donghong@student.42seoul.kr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 11:59:57 by donghong          #+#    #+#             */
+/*   Updated: 2023/05/16 12:00:07 by donghong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 void	put_img(t_map *map, int y, int x, char *file)
@@ -11,44 +23,55 @@ void	put_img(t_map *map, int y, int x, char *file)
 	mlx_destroy_image(map->mlx, img);
 }
 
-int	show_map(t_map *map, int x, int y)
+void	base_map(t_map *map)
 {
+	int	x;
+	int	y;
+
+	x = 0;
 	mlx_clear_window(map->mlx, map->win);
 	while (x < map->height)
 	{
 		y = 0;
 		while (y < map->width)
 		{
-			if (map->map_down[x][y] == '0')
-				put_img(map, y, x, "./pic/base.xpm");
-			else if (map->map_down[x][y] == '1')
-			{
-				put_img(map, y, x, "./pic/base.xpm");
-				put_img(map, y, x, "./pic/turret.xpm");
-			}
-			else if (map->map_down[x][y] == 'E' && map->c != 0)
-			{
-				put_img(map, y, x, "./pic/base.xpm");
-				put_img(map, y, x, "./pic/exit1.xpm");
-			}
-			else if (map->map_down[x][y] == 'E' && map->c == 0)
-			{
-				put_img(map, y, x, "./pic/base.xpm");
-				put_img(map, y, x, "./pic/exit2.xpm");
-			}
-			else if (map->map_down[x][y] == 'C')
-			{
-				put_img(map, y, x, "./pic/base.xpm");
-				put_img(map, y, x, "./pic/object.xpm");
-			}
-			else if (map->map_down[x][y] == 'P')
-			{
-				put_img(map, y, x, "./pic/base.xpm");
-				put_img(map, y, x, "./pic/monster.xpm");
-			}
+			put_img(map, y, x, "./pic/base.xpm");
 			y++;
 		}
 		x++;
 	}
+}
+
+void	place_images(t_map *map)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < map->height)
+	{
+		y = 0;
+		while (y < map->width)
+		{
+			if (map->map_down[x][y] == '1')
+				put_img(map, y, x, "./pic/turret.xpm");
+			else if (map->map_down[x][y] == 'E' && map->c != 0)
+				put_img(map, y, x, "./pic/exit1.xpm");
+			else if (map->map_down[x][y] == 'E' && map->c == 0)
+				put_img(map, y, x, "./pic/exit2.xpm");
+			else if (map->map_down[x][y] == 'C')
+				put_img(map, y, x, "./pic/object.xpm");
+			else if (map->map_down[x][y] == 'P')
+				put_img(map, y, x, "./pic/monster.xpm");
+			y++;
+		}
+		x++;
+	}
+}
+
+int	show_map(t_map *map, int x, int y)
+{
+	base_map(map);
+	place_images(map);
 	return (0);
 }

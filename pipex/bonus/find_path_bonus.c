4cbc08f7954fcdd3725538ec2fb87da14b63bc92
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2023-06-20 22:54:09 +0900

    Add files via upload

--- pipex/bonus/find_path_bonus.c
+++ pipex/bonus/find_path_bonus.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   find_path.c                                        :+:      :+:    :+:   */
+/*   bonus_find_path.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: donghong <donghong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 19:39:37 by donghong          #+#    #+#             */
-/*   Updated: 2023/06/14 14:26:45 by donghong         ###   ########.fr       */
+/*   Updated: 2023/06/14 16:23:21 by donghong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "pipex_bonus.h"
 
 char	**get_path_envp(char **env)
 {
 	char	*path;
 
-	path = NULL;
-	if (*env == NULL)
-		return (NULL);
 	while (ft_strncmp("PATH=", *env, 5))
 		env++;
+	if (*env == NULL)
+		return (NULL);
 	path = *env + 5;
 	if (path == NULL)
 		return (NULL);
@@ -39,7 +38,7 @@ void	ft_path(char **env, t_base *base)
 	while (base->path[i])
 	{
 		temp = base->path[i];
-		base->path[i] = ft_strjoin(temp, "/");
+		base->path[i] = ft_strjoin_bonus(temp, "/");
 		free(temp);
 		i++;
 	}
@@ -83,7 +82,7 @@ char	*search_path(t_base *base)
 	k = 0;
 	while (base->path[k])
 	{
-		tmp = ft_strjoin(base->path[k], base->cmd_abs[0]);
+		tmp = ft_strjoin_bonus(base->path[k], base->cmd_abs[0]);
 		if (access(tmp, X_OK) == 0)
 			return (tmp);
 		free(tmp);
---
+++ pipex/bonus/heredoc_bonus.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus_heredoc.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donghong <donghong@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 23:23:52 by donghong          #+#    #+#             */
+/*   Updated: 2023/06/14 16:26:11 by donghong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+int	check_exist(char **av)
+{
+	if (ft_strncmp("here_doc", av[1], 9) == 0)
+		return (1);
+	return (0);
+}
+
+void	here_doc(t_here *here)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = get_next_line(STDIN_FILENO);
+	if (!str)
+		return ;
+	if ((ft_strncmp(here->limit, str, here->len) == 0) && \
+		(here->len == (ft_strlen(str) - 1)))
+	{
+		free(str);
+		return ;
+	}
+	while (str)
+	{
+		write(here->infile, str, ft_strlen(str));
+		free(str);
+		str = get_next_line(STDIN_FILENO);
+		if ((ft_strncmp(here->limit, str, here->len) == 0) && \
+			(here->len == (ft_strlen(str) - 1)))
+		{
+			free(str);
+			return ;
+		}
+	}
+}
+
+void	again(t_here *here)
+{
+	close(here->infile);
+	here->infile = open("heredoc", O_RDONLY);
+	if (here->infile == -1)
+		perror("File fail");
+}
+
+void	combine(t_here *here)
+{
+	here_doc(here);
+	again(here);
+}
